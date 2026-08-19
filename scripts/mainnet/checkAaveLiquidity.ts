e6bf9a945ed7b6b639200319630f2f1b7543b1f7
Pikyoe <yudiw4629@gmail.com>
2026-08-19 23:30:01 +0700

    Update checkAaveLiquidity.ts

--- scripts/mainnet/checkAaveLiquidity.ts
+++ scripts/mainnet/checkAaveLiquidity.ts
@@ -38,10 +38,10 @@ const ERC20_ABI = [
 
 const TOKENS: { name: string; address: string; decimals: number }[] = [
     { name: "WETH", address: "0x4200000000000000000000000000000000000006", decimals: 18 },
-    { name: "USDC", address: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913", decimals: 6 },
+    { name: "USDC", address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", decimals: 6 },
     { name: "USDbC", address: "0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca", decimals: 6 },
-    { name: "cbBTC", address: "0xcbb7c0000ab88b473b1f5afd5782588411511c8c", decimals: 8 },
-    { name: "AERO", address: "0x940181a94a35a4569e4529a3cdfb74e38fd98631", decimals: 18 },
+    { name: "cbBTC", address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf", decimals: 8 },
+    { name: "AERO", address: "0x940181a94A35A4569E4529A3CDfB74e38FD98631", decimals: 18 },
     { name: "EURC", address: "0x60a3e35cc302bfa44cb288bc5a4f316fdb1adb42", decimals: 6 },
     { name: "wstETH", address: "0xc1cba3fcea344f92d9239c08c0568f6f2f0ee452", decimals: 18 },
 ];
